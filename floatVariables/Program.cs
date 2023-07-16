5d6c0b31f49b80e85c8b283358bcff79bb559698
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-16 18:21:54 +0300

    Proje dosyası ekle.

---
+++ floatVariables/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace floatVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //float sayi;
+            //sayi = 445.57f;
+            //Console.WriteLine(sayi);
+
+            //Console.Read();
+
+            float s1, s2, sum;
+            Console.WriteLine("Lütfen bir sayi giriniz : ");
+            s1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Lütfen ikinci sayıyı girin : ");
+            s2 = float.Parse(Console.ReadLine());
+            sum = s1 + s2;
+            Console.WriteLine("Toplam : " + sum);
+            Console.Read();
+        }
+    }
+}
